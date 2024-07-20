089a499180a93bfa3926b22b2132bf4cbbd62b20
Hamza chadili <chadilisafi@gmail.com>
2024-07-20 17:39:09 +0100

    execution

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: hchadili <hchadili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:40:16 by hchadili          #+#    #+#             */
-/*   Updated: 2024/07/19 22:54:41 by hchadili         ###   ########.fr       */
+/*   Updated: 2024/07/20 17:33:51 by hchadili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,38 @@
 void ft_cd(t_cmd *cmnd, t_env **env)
 {
 	(void)env;
+	(void)cmnd;
 	char arr[PATH_MAX];
 	t_env *tmp;
 	t_env *tmp2;
 	tmp = *env;
 	tmp2 = *env;
+	int x = 0;
 	while (tmp)
 	{
-		if(strcmp(tmp->key ,"HOME") == 0)
+		if (strcmp(tmp->key, "HOME") == 0)
 			break;
 		tmp = tmp->next;
 	}
-	if(!cmnd->cmds[2])
-	{	
-		if(!cmnd->cmds[1])
-			chdir(tmp->value);
-		else if(chdir(cmnd->cmds[1]))
+	while (cmnd->cmds[x])
+		x++;
+	if (x == 1)
+	{
+		if (chdir(tmp->value))
 		{
 			perror("chdir erorr");
-			return ;
+			return;
 		}
-		tmp = *env;
+		tmp = *env;	
 		while (tmp)
 		{
-			if(strcmp(tmp->key ,"PWD") == 0)
+			if (strcmp(tmp->key, "PWD") == 0)
 				break;
 			tmp = tmp->next;
 		}
 		while (tmp2)
 		{
-			if(strcmp(tmp2->key ,"OLDPWD") == 0)
+			if (strcmp(tmp2->key, "OLDPWD") == 0)
 				break;
 			tmp2 = tmp2->next;
 		}
@@ -55,6 +57,30 @@ void ft_cd(t_cmd *cmnd, t_env **env)
 			tmp->value = ft_strdup_env(arr);
 	}
 	else
-		perror("chdir erorr");
+	{
+		if (chdir(cmnd->cmds[1]) || x != 2)
+		{
+			perror("chdir erorr");
+			return;
+		}
+		tmp = *env;
+		while (tmp)
+		{
+			if (strcmp(tmp->key, "PWD") == 0)
+				break;
+			tmp = tmp->next;
+		}
+		while (tmp2)
+		{
+			if (strcmp(tmp2->key, "OLDPWD") == 0)
+				break;
+			tmp2 = tmp2->next;
+		}
+		if (tmp2 && tmp)
+			tmp2->value = tmp->value;
+		getcwd(arr, sizeof(arr));
+		if (tmp)
+			tmp->value = ft_strdup_env(arr);
+
+	}
 }
- 
